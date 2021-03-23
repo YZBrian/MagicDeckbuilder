fc63d357d49fca821612b694fe6d62fe018b2180
Brian Hutjens <57947324+YZBrian@users.noreply.github.com>
2021-03-23 11:54:52 +0100

    Commit 2

---
+++ MagicDeckbuilder.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MagicDeckbuilder.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ MagicDeckbuilder.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MagicDeckbuilder.Logic
+{
+    public class Class1
+    {
+    }
+}
