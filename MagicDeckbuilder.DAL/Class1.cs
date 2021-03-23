e87bfa9767a4d05610e3f85678a64a3a3685412a
Brian Hutjens <57947324+YZBrian@users.noreply.github.com>
2021-03-23 12:16:09 +0100

    Created Card Overview Page

--- MagicDeckbuilder.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MagicDeckbuilder.DAL
-{
-    public class Class1
-    {
-    }
-}
---
+++ MagicDeckbuilder.Web/Controllers/CardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicDeckbuilder.Controllers
+{
+    public class CardController : Controller
+    {
+        // GET
+        public IActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
